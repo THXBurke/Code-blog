f32eaf4326878f724daf90b2856c47a3f209e455
THXBurke <rachelwheels@gmail.com>
2016-01-20 09:24:36 -0800

    skeleton

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,16 @@
-var blogPosts = [
+var blogPostData = [
   {
     title:       'Blog Post #1',
     category:    'Code Fellows Assignment',
     author:      'Rachel Burke',
     publishedOn: 'January 1, 2016',
-    body:        '<p></p>',
+    body:        '<p>Content Here</p>',
   },
   {
     title:       'Blog Post #2',
     category:    'More learning how to code',
     author:      'Rachel Burke',
     publishedOn: 'January 15, 2016',
-    body:        '<p></p>',
+    body:        '<p>Content Here</p>',
   }
 ];
