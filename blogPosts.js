7ec80708439b5419f6890d0e87bfba2d1f018f1d
THXBurke <rachelwheels@gmail.com>
2016-01-20 12:26:39 -0800

    connect html to js

--- blogPosts.js
+++ blogPosts.js
@@ -1,3 +1,5 @@
+
+
 var blogPosts = [];
 
 function blogPost (options) {
@@ -9,33 +11,21 @@ function blogPost (options) {
 }
 
 blogPost.prototype.toHtml = function() {
-  var $newblogPost = $('blogPost.template').clone();
+  $newblogPost = $('blogPost.template').clone();
   $newblogPost.removeClass('template');
-  if (!this.publishedOn) {
-    $newblogPost.addClass('draft');
-  }
+  $newblogPost.find('about-me').text(this.title);
+  $newblogPost.find('time').text(this.publishedOn);
+  $newblogPost.find('.blogBody').attr('data-category', this.category);
+  $newblogPost.find('.blogBody').html(this.body);
 
-  $newblogPost.attr('data-category', this.category);
-
-
-  $newblogPost.find('.byline a').html(this.author);
-  $newblogPost.find('h1:first').html(this.title);
-  $newblogPost.find('blogPost-body').html(this.body);
-  $newblogPost.find('time[pubdate]').attr('datetime', this.publishedOn);
-  $newblogPost.find('time[pubdate]').attr('title', this.publishedOn);
-  $newblogPost.find('time').html('about ' + parseInt((new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  $newblogPost.appendd('<hr>');
   return $newblogPost;
 };
 
-blogPosts.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+blogPostData.forEach(function(p) {
+  blogPosts.push(new blogPost(p));
 });
 
-blogPostData.forEach(function(ele) {
-  blogPosts.push(new blogPosts(ele));
-});
-
-blogPosts.forEach(function(a) {
-  $('#blogPosts').append(a.toHtml());
+blogPosts.forEach(function(e) {
+  $blogPosts = $('#blogPosts');
+  $blogPosts.append(e.toHtml());
 });
