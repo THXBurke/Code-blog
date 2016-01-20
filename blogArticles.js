74cde9b357ab8e24e9dba3cd23a518e18607375c
THXBurke <rachelwheels@gmail.com>
2016-01-19 21:16:02 -0800

    update on all

--- blogArticles.js
+++ blogArticles.js
@@ -1,75 +1,41 @@
-var articles = [];
+var blogPosts = [];
 
-function Article (options) {
+function blogPost (options) {
   this.title = options.title;
   this.category = options.category;
   this.author = options.author;
-  this.authorUrl = options.authorUrl;
   this.publishedOn = options.publishedOn;
   this.body = options.body;
 }
 
-Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.attr('category', this.category);
-  $newArticle.attr('data-category', this.category);
-  //finds the anchor tag and updates it and gives it an hfref value. .text chained for less code
-  $newArticle.find('a').attr('href', this.authorUrl).text(this.author);
-  $newArticle.find('h1').text(this.title);//finds any h1 tag to change text whenever needed
-  $newArticle.find('section').html(this.body);//finds the section and puts the body data in it that's stored as html
-  $newArticle.find('time[pubdate]').attr('pubdate', this.publishedOn);//creates a new pubdate attr using the publishedOn data
-
-  //Includes the pub date as a 'title'attr to show on hover
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-
-  //Displays the date as a relative number of "days ago"
-  $newArticle.find('time').html('about' + parseInt((new Data() - new Data(this.publishedOn))/60/60/24/1000) + 'days ago');
-
-  $newArticle.append('<hr>');
-
-  $newArticle.removeClass('template');//removes template class because it is only an instance of the template being used
-
-  return $newArticle;
-};
-
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date( a.publishedOn));
-});
-
-rawData.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
-
-articles.forEach(function(a) {
-  $('articles').append(a.toHtml());
-});
-
-
-
-blog.rawData = [
-  {
-    title:       'Blog Post #1',
-    category:    'Learning how to code',
-    author:      'Rachel Burke',
-    authorUrl:   '',
-    publishedOn: '2016-01-11',
-    body:        '<p>}',
-  },
-  {
-    title:       'Blog Post #2',
-    category:    'More learning how to code',
-    author:      'Rachel Burke',
-    authorUrl:   '',
-    publishedOn: '',
-    body:        '<p>}',
-  },
-  {
-    title:       'Blog Post #3',
-    category:    'More learning how to code',
-    author:      'Rachel Burke',
-    authorUrl:   '',
-    publishedOn: '',
-    body:        '<p>}',
+blogPost.prototype.toHtml = function() {
+  var $newblogPost = $('blogPost.template').clone();
+  $newblogPost.removeClass('template');
+  if (!this.publishedOn) {
+    $newblogPost.addClass('draft');
   }
 
-];
+  $newblogPost.attr('data-category', this.category);
+
+
+  $newblogPost.find('.byline a').html(this.author);
+  $newblogPost.find('h1:first').html(this.title);
+  $newblogPost.find('blogPost-body').html(this.body);
+  $newblogPost.find('time[pubdate]').attr('datetime', this.publishedOn);
+  $newblogPost.find('time[pubdate]').attr('title', this.publishedOn);
+  $newblogPost.find('time').html('about ' + parseInt((new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newblogPost.appendd('<hr>');
+  return $newblogPost;
+};
+
+blogPosts.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+blogPost.forEach(function(ele) {
+  blogPosts.push(new Article(ele));
+});
+
+blogPosts.forEach(function(a) {
+  $('#blogPosts').append(a.toHtml());
+});
---
+++ portfolio.js
@@ -0,0 +1,16 @@
+var blogPosts = [
+  {
+    title:       'Blog Post #1',
+    category:    'Code Fellows Assignment',
+    author:      'Rachel Burke',
+    publishedOn: 'January 1, 2016',
+    body:        '<p></p>',
+  },
+  {
+    title:       'Blog Post #2',
+    category:    'More learning how to code',
+    author:      'Rachel Burke',
+    publishedOn: 'January 15, 2016',
+    body:        '<p></p>',
+  }
+];
